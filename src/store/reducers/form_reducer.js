d640f26294fad8e0d3e87aa0215c993009868252
Saint Jordi <jordiws@gmail.com>
2021-03-06 18:43:31 -0300

    reducers ok

--- src/store/reducers/form_reducer.js
+++ src/store/reducers/form_reducer.js
@@ -1,2 +1,30 @@
 //Will hold the reducer to read and update state properties related to our form
 
+import * as ACTION_TYPES from '../actions/action_types'
+
+export const initialState = {
+    user_textChange: "",
+    user_textSubmit: ""
+}
+
+export const FormReducer = (state, action) => {
+    switch (action.type){
+        case ACTION_TYPES.USER_INPUT_SUBMIT:
+            return {
+                ...state,
+                user_textSubmit: action.payload
+            }
+        case ACTION_TYPES.USER_INPUT_CHANGE:
+            return {
+                ...state,
+                user_textChange: action.payload
+            }
+        default:
+            throw new Error();
+    }
+}
+
+/*
+* Here we have 2 properties for a form. Our first property tracks changes to the input element and
+*  our second property adds the submitted form to the global state.
+* */

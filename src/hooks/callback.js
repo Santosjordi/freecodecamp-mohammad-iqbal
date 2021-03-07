76e24425c922d413e9159216fab07c0df5d0447d
Saint Jordi <jordiws@gmail.com>
2021-03-06 21:10:27 -0300

    header ok

--- src/hooks/callback.js
+++ src/hooks/callback.js
@@ -1,2 +1,11 @@
 //will be used as the component that Auth0 will redirect to after the user authenticates
 
+import React from 'react'
+
+const Callback = props => (
+    <div>
+        Callback
+    </div>
+);
+
+export default Callback;
--- src/hooks/header.js
+++ src/hooks/header.js
@@ -1,2 +1,34 @@
 //will contain the links to the components and a login or logout button based on the user authentication state
 
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import Context from '../utils/context';
+
+const Header = () => {
+    const context = useContext(Context)
+
+    return (
+        <div>
+            <Link to='/' style={{padding: "5px"}}>
+                Profile
+            </Link>
+            <link to='/profile' style={{padding: "5px"}}>
+                Profile
+            </link>
+            <Link to='/hooksform' style={{padding: "5px"}}>
+                Hooks Form
+            </Link>
+            <Link to='/hookscontainer' style={{padding: "5px"}}>
+                Hooks Container
+            </Link>
+            <Link to='/privateroute' style={{padding: "5px"}}>
+                Private Route
+            </Link>
+            {!context.authState
+                ? <button onClick={() => context.authObj.login()}>Login</button>
+                : <button onClick={() => context.authObj.logout()}>Login</button>
+            }
+        </div>
+    )};
+
+export default Header;
